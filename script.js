194350b3a14afe3c34839ab9ec6c73d504ad664b
zoklukealt <78869199+zoklukealt@users.noreply.github.com>
2021-04-19 11:47:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,40 +2,31 @@
 // If you use this channel ID your app will stop working in the future
 const CLIENT_ID = 'vOAIBkmBfguAFa44';
 var txt;
-
-
-function getCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
     }
-    else
-    {
-        begin += 2;
-        var end = document.cookie.indexOf(";", begin);
-        if (end == -1) {
-        end = dc.length;
-        }
-    }
-    return decodeURI(dc.substring(begin + prefix.length, end));
-} 
-
-function doSomething() {
-    var myCookie = getCookie("username");
-
-    if (myCookie == null) {
-      const person = prompt("please enter your name. it will be cut short if it is 20+ characters!");
-      const person = person.substring(0, 20);
-    }
-    else {
-      document.cookie = `username=${person}`
-    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
-
-doSomething()
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+if (getCookie("user") == null) {
+    prompt("enter name loser")
+    setCookie("user",person,30);
+} else {
+    const person = getCookie("user")
+}
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
